94792c2b692bf6aabb8441c73945b5185342022d
Matthew B. Gray <code@matthew.nz>
2017-10-11 16:35:00 +1300

    Tidyup whitespace, update readme

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # site-status-gem
-Checks the status of https://gitlab.com and prints out how long it took to respond
+Checks the status of a given site and prints out how long it took to respond
 
 ## Usage
 Installs a binary called `site-status` that you can call against any site.
 
 What you get back is response time in milliseconds and response code.
 
+If you don't provide an argument, it defaults to https://gitlab.com
+
 e.g.
 ```
+site-status
+=> 302 20ms
+
 site-status https://matthew.nz
 => 302 30ms
 ```
--- lib/uptime/status_checker.rb
+++ lib/uptime/status_checker.rb
@@ -7,37 +7,37 @@ module Uptime
     def call
       check_errors
       return nil if errors.any?
-  
+
       DataPoint.new.tap do |data|
         bench = Benchmark.measure do
           data[:response] = response
         end
-  
+
         data[:time] = bench.total
       end
     end
-  
+
     def errors
       @errors ||= []
     end
-  
+
     private
-  
+
     def check_errors
       if site.nil?
         errors << "Site cannot be nil"
         return # basic failure, just stop processing
       end
-  
+
       if site_uri.host.nil?
         errors << "Cannot parse #{site}, invalid format"
       end
     end
-  
+
     def response
       Net::HTTP.get_response(site_uri)
     end
-  
+
     def site_uri
       URI(site)
     end
